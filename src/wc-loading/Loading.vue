5733fdbe559968f8b13f46d2b602efd0b61bb3d9
林丰 <linhui@linfengdeMacBook-Air.local>
2017-12-17 01:14:37 +0800

    完成了 loading 的效果.

--- src/wc-loading/Loading.vue
+++ src/wc-loading/Loading.vue
@@ -1,9 +1,51 @@
-<style>
-	
+<style lang="less">
+@import './loading.less';
+.wc-loading-wrapper {
+    position: fixed;
+    z-index: 99999;
+    width: 91px;
+    min-height: 91px;
+    background: rgba(0, 0, 0, 0.7);
+    text-align: center;
+    border-radius: 5px;
+    color: #fff;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.wc-loading__loading-wrapper {
+	position: absolute;
+	top: 38%;
+	left: 50%;
+}
+.wc-loading__text {
+	font-size: 14px;
+	color: white;
+	position: absolute;
+	bottom: 10px;
+	text-align: center;
+	width: 100%;
+}
 </style>
 <template>
 	<div class="wc-loading-wrapper">
-		你好222
+		<div class="wc-loading__loading-wrapper">
+			<div class="loading_leaf loading_leaf_0"></div>
+			<div class="loading_leaf loading_leaf_1"></div>
+			<div class="loading_leaf loading_leaf_2"></div>
+			<div class="loading_leaf loading_leaf_3"></div>
+			<div class="loading_leaf loading_leaf_4"></div>
+			<div class="loading_leaf loading_leaf_5"></div>
+			<div class="loading_leaf loading_leaf_6"></div>
+			<div class="loading_leaf loading_leaf_7"></div>
+			<div class="loading_leaf loading_leaf_8"></div>
+			<div class="loading_leaf loading_leaf_9"></div>
+			<div class="loading_leaf loading_leaf_10"></div>
+			<div class="loading_leaf loading_leaf_11"></div>	
+		</div>
+		<div class="wc-loading__text">
+			正在加载
+		</div>
 	</div>
 </template>
 <script>
